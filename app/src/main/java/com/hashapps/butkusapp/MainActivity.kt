a2ff33a05d70e1f0492b867d6c9bbb1034bb6a78
Hari Menon <hari@galois.com>
2022-12-02 14:36:49 -0500

    Integrate ButkusCore
    A (configurable) Pytorch mobile model is now included with the assets.
    Additionally introduce a Butkus class to facilitate integration with ButkusCore

--- app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
+++ app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.hashapps.butkusapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Initialize Butkus asynchronously
+        //TODO: This doesn't quite do what we want
+        val butkusInit = GlobalScope.async { Butkus.initialize(applicationContext) }
+
         setContentView(R.layout.activity_main)
+
+        //Wait for butkus to be done?
+        runBlocking { butkusInit.await() }
+
+        GlobalScope.async {
+            val text = Butkus.getInstance().encode("Hello")
+            println(text)
+        }
     }
 }
