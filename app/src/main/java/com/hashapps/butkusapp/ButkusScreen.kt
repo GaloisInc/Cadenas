206d6c43cafe112fb99db6231fd11752101aacac
Chris Phifer <cphifer@galois.com>
2022-12-04 01:05:19 -0800

    Begin implementing the screen navigation/top-level UI structures.
    Butkus consists of two screens: The encoder and the decoder. This
    commit adds a simple navigation bar, and sets the stage for the
    implementation of the actual screens enabling text encoding.

---
+++ app/src/main/java/com/hashapps/butkusapp/ButkusScreen.kt
@@ -0,0 +1,83 @@
+package com.hashapps.butkusapp
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.hashapps.butkusapp.ui.ButkusViewModel
+
+enum class ButkusScreen(@StringRes val title: Int) {
+    Encode(title = R.string.encode),
+    Decode(title = R.string.decode),
+}
+
+@Composable
+fun ButkusAppBar(
+    currentScreen: ButkusScreen,
+    canSwitchScreen: Boolean,
+    switchScreen: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canSwitchScreen) {
+                IconButton(onClick = switchScreen) {
+                    Icon(
+                        imageVector = Icons.Filled.Refresh,
+                        contentDescription = stringResource(R.string.switch_button),
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun ButkusApp(
+    modifier: Modifier = Modifier,
+    viewModel: ButkusViewModel = ButkusViewModel(),
+) {
+    val navController = rememberNavController();
+
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentScreen = ButkusScreen.valueOf(
+        backStackEntry?.destination?.route ?: ButkusScreen.Encode.name
+    )
+
+    Scaffold(
+        topBar = {
+            ButkusAppBar(
+                currentScreen = currentScreen,
+                canSwitchScreen = true,
+                switchScreen = { /* TODO: Implement switch-screen button */ }
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+
+        NavHost(
+            navController = navController,
+            startDestination = ButkusScreen.Encode.name,
+            modifier = modifier.padding(innerPadding),
+        ) {
+            composable(route = ButkusScreen.Encode.name) {
+                // TODO: Composable for the encode screen
+            }
+
+            composable(route = ButkusScreen.Decode.name) {
+                // TODO: Composable for the decode screen
+            }
+        }
+    }
+}
