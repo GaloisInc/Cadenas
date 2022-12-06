b70679451510d28544ff099e518896f0820e6c29
Chris Phifer <cphifer@galois.com>
2022-12-06 15:39:09 -0800

    Implement an initial version of the decode screen

---
+++ app/src/main/java/com/hashapps/butkusapp/ui/DecodeScreen.kt
@@ -0,0 +1,61 @@
+package com.hashapps.butkusapp.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.hashapps.butkusapp.R
+import com.hashapps.butkusapp.data.DecodeUiState
+
+@Composable
+fun DecodeScreen(
+    decodeUiState: DecodeUiState,
+    onMessageChanged: (String) -> Unit,
+    onDecode: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = decodeUiState.message,
+            onValueChange = onMessageChanged,
+            singleLine = false,
+            label = { Text(stringResource(R.string.encoded_message_label)) },
+            placeholder = { Text(stringResource(R.string.encoded_message_placeholder)) },
+        )
+
+        Button(
+            modifier = modifier.fillMaxWidth(),
+            onClick = onDecode,
+        ) {
+            Text(
+                text = stringResource(R.string.decode),
+                style = MaterialTheme.typography.h6,
+            )
+        }
+
+        if (decodeUiState.decodedMessage != null) {
+            Divider(thickness = 2.dp, modifier = modifier)
+
+            TextField(
+                modifier = modifier.fillMaxWidth(),
+                value = decodeUiState.decodedMessage,
+                onValueChange = { },
+                readOnly = true,
+                label = { Text(stringResource(R.string.decode_output_label)) }
+            )
+        }
+    }
+}
