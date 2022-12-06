e9d41a4e75204aa6f4285cb0c8e8e8ef5c3c79b5
Chris Phifer <cphifer@galois.com>
2022-12-06 15:25:37 -0800

    Clean up MainActivity imports

--- app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
+++ app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
@@ -1,24 +1,9 @@
 package com.hashapps.butkusapp
 
-import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import com.hashapps.butkusapp.ui.theme.ButkusAppTheme
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
