d91847f19c26202ec88813454f319a6c7a395e91
Chris Phifer <cphifer@galois.com>
2022-12-08 15:02:14 -0800

    Delegate to viewModels so state is saved more intelligently

--- app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
+++ app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
@@ -3,14 +3,19 @@ package com.hashapps.butkusapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import com.hashapps.butkusapp.ui.ButkusViewModel
 import com.hashapps.butkusapp.ui.theme.ButkusAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModel: ButkusViewModel by viewModels()
+
         setContent {
             ButkusAppTheme {
-                ButkusApp()
+                ButkusApp(viewModel = viewModel)
             }
         }
     }
