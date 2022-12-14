2ea3239f5707021be6b3405368fcb25ecbb6133e
Chris Phifer <cphifer@galois.com>
2022-12-13 22:26:05 -0800

    Remove the no-longer-necessary canShare state variable

--- app/src/main/java/com/hashapps/butkusapp/data/DecodeUiState.kt
+++ app/src/main/java/com/hashapps/butkusapp/data/DecodeUiState.kt
@@ -7,9 +7,6 @@ data class DecodeUiState (
     /** Whether the drawer can be opened. */
     val canOpenDrawer: Boolean = true,
 
-    /** Whether the user can share to external apps. */
-    val canShare: Boolean = false,
-
     /** The message to decode. */
     val message: String = "",
 
