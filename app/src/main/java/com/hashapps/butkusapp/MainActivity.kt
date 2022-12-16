fe30694906fadf8f5f73c5a330a46324946dc954
Chris Phifer <cphifer@galois.com>
2022-12-15 16:14:57 -0800

    Receive text from shares (only works if the Butkus App activity has not already started)

--- app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
+++ app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hashapps.butkusapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +13,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val viewModel: ButkusViewModel by viewModels()
+
+        // If the activity was started via SEND intent with plaintext, update
+        // the viewModel to hold the sent text on the Decode screen
+        when (intent?.action) {
+            Intent.ACTION_SEND -> {
+                if (intent.type == "text/plain") {
+                    val message = intent.getStringExtra(Intent.EXTRA_TEXT)
+                    if (message != null) {
+                        viewModel.updateEncodedMessage(message)
+                    }
+                }
+            }
+            else -> {}
+        }
 
         setContent {
             ButkusAppTheme {
