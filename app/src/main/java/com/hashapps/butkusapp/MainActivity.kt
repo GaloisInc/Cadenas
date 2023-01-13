1b826ee623fcf4a00197434bcda1a7c50371f4b8
Chris Phifer <cphifer@galois.com>
2023-01-13 00:12:16 -0800

    Lock the app to portrait mode (prevents interruption of encoding/decoding on orientation change)

--- app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
+++ app/src/main/java/com/hashapps/butkusapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.hashapps.butkusapp
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import com.hashapps.butkusapp.ui.components.LockScreenOrientation
 import com.hashapps.butkusapp.ui.models.DecodeViewModel
 import com.hashapps.butkusapp.ui.models.EncodeViewModel
 import com.hashapps.butkusapp.ui.theme.ButkusAppTheme
@@ -16,6 +18,7 @@ class MainActivity : ComponentActivity() {
         val decodeViewModel: DecodeViewModel by viewModels()
 
         setContent {
+            LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
             ButkusAppTheme {
                 ButkusApp(
                     encodeViewModel = encodeViewModel,
