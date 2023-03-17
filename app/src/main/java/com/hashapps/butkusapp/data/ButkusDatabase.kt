5b20f8c78922573f79aa5eb303497a4a5a0d8135
Chris Phifer <cphifer@galois.com>
2023-03-17 02:32:15 -0700

    Set up the Room database.
    This simply "hooks up" the previously defined tables using their
    DAOs and the Room database builder. An instance is built if
    necessary in a typical way and provided via companion object for
    repository initialization.

---
+++ app/src/main/java/com/hashapps/butkusapp/data/ButkusDatabase.kt
@@ -0,0 +1,30 @@
+package com.hashapps.butkusapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.hashapps.butkusapp.data.model.Model
+import com.hashapps.butkusapp.data.model.ModelDao
+import com.hashapps.butkusapp.data.profile.Profile
+import com.hashapps.butkusapp.data.profile.ProfileDao
+
+@Database(entities = [Profile::class, Model::class], version = 2, exportSchema = false)
+abstract class ButkusDatabase : RoomDatabase() {
+    abstract fun profileDao(): ProfileDao
+
+    abstract fun modelDao(): ModelDao
+
+    companion object {
+        @Volatile
+        private var Instance: ButkusDatabase? = null
+
+        fun getDatabase(context: Context): ButkusDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, ButkusDatabase::class.java, "butkus_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
