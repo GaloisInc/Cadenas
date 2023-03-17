a246b7287b96e9567e9c1692a02c8fcd81f0234b
Chris Phifer <cphifer@galois.com>
2023-03-17 02:36:39 -0700

    Implement a repository providing the Butkus/Cadenas operations.
    Though it's a bit atypical, this repository provides indication
    of whether or not Butkus/Cadenas is initialized, and functionality
    to initialize the backend whenever the chosen profile changes.
    
    Additionally, this repository provides suspend functions to
    encode/decode via the Butkus/Cadenas object.
    
    The use of DataStore in this same repository saves the ID of the
    profile that's selected, which as mentioned above, is used to
    initialize Butkus/Cadenas (and, of course, to control the UI of
    the profile management screen.)

---
+++ app/src/main/java/com/hashapps/butkusapp/data/ButkusRepository.kt
@@ -0,0 +1,76 @@
+package com.hashapps.butkusapp.data
+
+import android.content.Context
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.hashapps.butkusapp.data.profile.Profile
+import com.hashapps.butkusapp.data.profile.ProfileDao
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import java.io.IOException
+
+private const val BUTKUS_SETTINGS_NAME = "butkus_settings"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = BUTKUS_SETTINGS_NAME
+)
+
+class ButkusRepository(
+    private val context: Context,
+    private val externalScope: CoroutineScope,
+    private val profileDao: ProfileDao,
+) {
+    private companion object {
+        val SELECTED_PROFILE = intPreferencesKey("selected_profile")
+
+        const val TAG = "SettingsRepo"
+    }
+
+    val selectedProfile: Flow<Int?> = context.dataStore.data
+        .catch {
+            if (it is IOException) {
+                Log.e(TAG, "Error reading settings.", it)
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }.map { preferences ->
+            preferences[SELECTED_PROFILE]
+        }
+
+    suspend fun saveSelectedProfile(selectedProfile: Int) {
+        context.dataStore.edit {
+            it[SELECTED_PROFILE] = selectedProfile
+        }
+    }
+
+    private var _butkusInitialized: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val butkusInitialized: StateFlow<Boolean> = _butkusInitialized.asStateFlow()
+
+    init {
+        externalScope.launch(Dispatchers.IO) {
+            val id = selectedProfile.filterNotNull().collectLatest {
+                val profile = profileDao.getProfile(it).first()
+                // TODO: Use the profile info the initialize Butkus
+                Butkus.initialize(context)
+                if (Butkus.getInstance() != null) {
+                    _butkusInitialized.update { true }
+                }
+            }
+        }
+    }
+
+    suspend fun encode(plaintext: String): String? = coroutineScope {
+        val encodedMessage = async(Dispatchers.Default) { Butkus.getInstance()?.encode(plaintext) }
+        encodedMessage.await()
+    }
+
+    suspend fun decode(encodedMessage: String): String? = coroutineScope {
+        val plaintext = async(Dispatchers.Default) { Butkus.getInstance()?.decode(encodedMessage) }
+        plaintext.await()
+    }
+}
