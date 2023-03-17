2b78cc07f8491d28d1a950a599712d5c14300b8f
Chris Phifer <cphifer@galois.com>
2023-03-17 02:58:30 -0700

    Provide a custom coroutine scope to the AppDataContainer for Butkus initialization

--- app/src/main/java/com/hashapps/butkusapp/ButkusApplication.kt
+++ app/src/main/java/com/hashapps/butkusapp/ButkusApplication.kt
@@ -3,12 +3,16 @@ package com.hashapps.butkusapp
 import android.app.Application
 import com.hashapps.butkusapp.data.AppContainer
 import com.hashapps.butkusapp.data.AppDataContainer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class ButkusApplication : Application() {
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
     lateinit var container: AppContainer
 
     override fun onCreate() {
         super.onCreate()
-        container = AppDataContainer(this)
+        container = AppDataContainer(applicationScope, this)
     }
 }
