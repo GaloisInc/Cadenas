2253b4aa30e29a5e1a236b9e91de58470bb967af
Chris Phifer <cphifer@galois.com>
2023-04-20 15:46:18 -0700

    Document the language model repository.

--- app/src/main/java/com/hashapps/cadenas/data/ModelRepository.kt
+++ app/src/main/java/com/hashapps/cadenas/data/ModelRepository.kt
@@ -7,6 +7,19 @@ import com.hashapps.cadenas.workers.ModelDownloadWorker
 import kotlinx.coroutines.flow.*
 import java.io.File
 
+/**
+ * Repository class for downloaded language models.
+ *
+ * Provides the means to fetch and delete language models, as well as a list of
+ * all models which have been previously downloaded.
+ *
+ * Rather than use another database table and duplicate the information, we
+ * rely on the device filesystem itself to act as the repository of language
+ * model truth.
+ *
+ * @property[modelDownloaderState] The state of the worker responsible for
+ * downloading models from the Internet
+ */
 class ModelRepository(
     private val modelsDir: File,
     private val workManager: WorkManager,
@@ -16,6 +29,14 @@ class ModelRepository(
         .asFlow()
         .map { it.getOrNull(0) }
 
+    /**
+     * Download a model from the Internet, and save it to disk with a given
+     * name. Work is performed asynchronously such that even application death
+     * will not cancel the download.
+     *
+     * @param[url] The HTTPS URL of the model ZIP
+     * @param[modelName] The name of the model/directory to store the model to
+     */
     fun downloadModelFromAndSaveAs(url: String, modelName: String) {
         val data = Data.Builder()
 
@@ -37,6 +58,13 @@ class ModelRepository(
             )
     }
 
+    /**
+     * Delete a model from the device. As with [downloadModelFromAndSaveAs],
+     * the work is done such that even application death will not cancel the
+     * deletion.
+     *
+     * @param[modelName] The name of the model/directory to delete
+     */
     fun deleteFilesForModel(modelName: String) {
         val toDeleteDir = modelsDir.resolve("$modelName.temp")
         modelsDir.resolve(modelName).renameTo(toDeleteDir)
@@ -55,6 +83,12 @@ class ModelRepository(
         workManager.enqueue(modelDeleteWOrkRequest)
     }
 
+    /**
+     * Return all models that have been downloaded to this device.
+     *
+     * @return The list of all model names that have been downloaded to the
+     * device
+     */
     fun downloadedModels() = modelsDir
         .listFiles()
         .orEmpty()
