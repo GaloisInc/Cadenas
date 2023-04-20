e15cf09247bc6c20a5f3c3bb32fc5170dd7129c2
Chris Phifer <cphifer@galois.com>
2023-04-20 15:05:48 -0700

    Document the top-level Cadenas application.

--- app/src/main/java/com/hashapps/cadenas/CadenasApplication.kt
+++ app/src/main/java/com/hashapps/cadenas/CadenasApplication.kt
@@ -4,6 +4,14 @@ import android.app.Application
 import com.hashapps.cadenas.data.AppContainer
 import com.hashapps.cadenas.data.AppDataContainer
 
+/**
+ * The _true_ entrypoint of the Cadenas application, constructed before
+ * [MainActivity].
+ *
+ * The [Application] houses an [AppContainer], holding references to all of the
+ * data sources driving the UI. This setup ensures that data is available from
+ * application startup.
+ */
 class CadenasApplication : Application() {
     lateinit var container: AppContainer
 
