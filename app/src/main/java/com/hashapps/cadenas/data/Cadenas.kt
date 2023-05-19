1885794abcc98d261052039898b12c5d0af0d75f
Chris Phifer <cphifer@galois.com>
2023-05-19 16:13:31 -0500

    Use ByteArray rather than String for secret key in configuration.

--- app/src/main/java/com/hashapps/cadenas/data/Cadenas.kt
+++ app/src/main/java/com/hashapps/cadenas/data/Cadenas.kt
@@ -51,13 +51,41 @@ class DecodeCache {
 
 data class CadenasConfig(
     val modelDir: String,
-    val key: String,
+    val key: ByteArray,
     val seed: String,
     val temperature: Float = 1.0f,
     val topK: Int = 100,
     val topP: Float = 0.0f,
     val indexMax: Int = 5
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CadenasConfig
+
+        if (modelDir != other.modelDir) return false
+        if (!key.contentEquals(other.key)) return false
+        if (seed != other.seed) return false
+        if (temperature != other.temperature) return false
+        if (topK != other.topK) return false
+        if (topP != other.topP) return false
+        if (indexMax != other.indexMax) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = modelDir.hashCode()
+        result = 31 * result + key.contentHashCode()
+        result = 31 * result + seed.hashCode()
+        result = 31 * result + temperature.hashCode()
+        result = 31 * result + topK
+        result = 31 * result + topP.hashCode()
+        result = 31 * result + indexMax
+        return result
+    }
+}
 
 class Cadenas(config: CadenasConfig) {
 
