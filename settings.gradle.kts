8e47ba78902534238bf69b8496e14ecb34b41da4
Chris Phifer <cphifer@galois.com>
2023-08-04 22:36:57 -0700

    Use Kotlin instead of Groovy for settings.gradle.

---
+++ settings.gradle.kts
@@ -0,0 +1,52 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    plugins {
+        id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+        id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
+    }
+}
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+rootProject.name = "Cadenas"
+
+include(":app")
+include(":cadenas-core")
+
+include("shared", "api", "services:webservice")
+
+val artifactoryUrl: String by settings
+val artifactoryUser: String by settings
+val artifactoryPassword: String by settings
+
+val cachePush: String by settings
+
+buildCache {
+    local {
+        isEnabled = false
+    }
+    remote<HttpBuildCache> {
+        url = uri("$artifactoryUrl/cadenas_generic-local/")
+        credentials {
+            username = artifactoryUser
+            password = artifactoryPassword
+        }
+        isPush = cachePush == "true"
+    }
+}
