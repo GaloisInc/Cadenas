d53d9e675b0c61f73471629d45e9d7357a57b6f9
Chris Phifer <cphifer@galois.com>
2023-08-22 17:29:52 -0700

    CadenasApplication.kt: Create a notification channel.
    If the app is built for SDK >= 26, create a notification channel to
    be used by the application.
    
    Note that as of this time, this is a redundant check, since the
    application has a minimum SDK of 26 (Android 8.) The check is
    included in case we wish to support even older versions of Android
    in the future.

--- app/src/main/java/com/hashapps/cadenas/CadenasApplication.kt
+++ app/src/main/java/com/hashapps/cadenas/CadenasApplication.kt
@@ -1,6 +1,10 @@
 package com.hashapps.cadenas
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.hashapps.cadenas.data.AppContainer
 import com.hashapps.cadenas.data.AppDataContainer
 
@@ -17,6 +21,23 @@ class CadenasApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
         container = AppDataContainer(this)
+    }
+
+    companion object {
+        const val CHANNEL_ID: String = "model_management"
     }
 }
