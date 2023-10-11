c7a171746ec02b65dadfd15da1134f5abe58cd16
Chris Phifer <cphifer@galois.com>
2023-10-11 16:24:01 -0700

    Use an alpha release of material3 for segmented buttons.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     platform("androidx.compose:compose-bom:2023.10.00")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha09")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.material:material-icons-core")
