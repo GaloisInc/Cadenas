54e03e0f08969f9053d59a1ca869214b67c2c3ee
Chris Phifer <cphifer@galois.com>
2023-10-12 23:52:12 -0700

    Add a text/plain deep link to the processing screen.
    In order to support sharing to Cadenas (for decoding), we must add
    a deepLink object to the definition of the navigation graph on the
    processing screen route.
    
    This commit adds such an object to the appropriate navigation
    route.

--- app/src/main/java/com/hashapps/cadenas/ui/processing/ProcessingNavigation.kt
+++ app/src/main/java/com/hashapps/cadenas/ui/processing/ProcessingNavigation.kt
@@ -1,16 +1,26 @@
 package com.hashapps.cadenas.ui.processing
 
+import android.content.Intent
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 
 const val PROCESSING_ROUTE = "processing"
 
 fun NavGraphBuilder.processingScreen(
     onNavigateToSettings: () -> Unit,
 ) {
-    composable(PROCESSING_ROUTE) {
+    composable(
+        route = PROCESSING_ROUTE,
+        deepLinks = listOf(
+            navDeepLink {
+                action = Intent.ACTION_SEND
+                mimeType = "text/plain"
+            }
+        ),
+    ) {
         ProcessingScreen(onNavigateToSettings = onNavigateToSettings)
     }
 }
