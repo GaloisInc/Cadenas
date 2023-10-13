979c291057d4c776e8eaeab1cc9a7f6e682f8185
Chris Phifer <cphifer@galois.com>
2023-10-13 10:01:14 -0700

    Revert "Increase version code (significant interface changes)."
    This reverts commit 79204a812d4f309d7f46e3403cafe76abc6ec4dc.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.hashapps.cadenas"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
