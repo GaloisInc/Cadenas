db41bf6af6c98cb11f86199161490f7146f32f13
Chris Phifer <cphifer@galois.com>
2023-10-21 20:09:00 -0700

    Change callback type to be more idiomatic, close the images in the correct place.

--- app/src/main/java/com/hashapps/cadenas/data/QRAnalyzer.kt
+++ app/src/main/java/com/hashapps/cadenas/data/QRAnalyzer.kt
@@ -11,28 +11,31 @@ import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
 class QRAnalyzer(
-    private val onCodeScanned: (Barcode) -> Unit,
+    private val onCodeScanned: (List<Barcode>) -> Unit,
 ) : ImageAnalysis.Analyzer {
     @OptIn(ExperimentalGetImage::class) override fun analyze(imageProxy: ImageProxy) {
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+        val scanner = BarcodeScanning.getClient(options)
+
         val mediaImage = imageProxy.image
         mediaImage?.let {
-            val options = BarcodeScannerOptions.Builder()
-                .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-                .build()
-
-            val scanner = BarcodeScanning.getClient(options)
             val image = InputImage.fromMediaImage(it, imageProxy.imageInfo.rotationDegrees)
 
             scanner.process(image)
                 .addOnSuccessListener { barcodes ->
                     if (barcodes.isNotEmpty()) {
-                        onCodeScanned(barcodes.first())
+                        onCodeScanned(barcodes)
                     }
                 }
                 .addOnFailureListener { exception ->
                     Log.e("QRAnalyzer", "QRAnalyzer: Something went wrong ($exception)")
                 }
+                .addOnCompleteListener {
+                    mediaImage.close()
+                    imageProxy.close()
+                }
         }
-        imageProxy.close()
     }
 }
