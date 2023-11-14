8288a4a4d0e31889b6fa1184fb61e2f54b599943
Chris Phifer <cphifer@galois.com>
2023-11-14 15:24:59 -0800

    ModelDownloadWorker: Add more robust error-reporting.
    First, a minor change that has no observable effects: We now
    subclass Worker instead of CoroutineWorker, since we have no need
    for coroutines in this operation - the work is already being done
    in a separate thread.
    
    Previously, the model-downloading worker did not provide very good
    responses to the user upon failure, and indeed, some cases would
    fail without reporting anything at all.
    
    This commit rectifies this sorry state of affairs, adding checks
    for (in theory) all possible exceptions that may occur when trying
    to download a model from the Internet. In particular:
    
    - We fail if an exception occurs while setting up a connection in
      the first place. It is unlikely that this will ever occur, as
      the UI prevents invalid HTTPS URLs from being entered.
    - We fail if actually establishing a connection fails. This check
      covers cases where, for instance, a syntactically valid but
      nonsense HTTPS URL is entered. In the special case of a timeout,
      we appropriately indicate that is what occurred.
    - We fail if the content-type header is not application/zip. This
      may be too conservative a check, since it is permissible for the
      content-type header to be absent in an HTTP(S) record. We can
      easily remove this check if it proves too aggressive for client
      needs.
    - We fail if an input stream cannot be created from the established
      connection.
    - We fail if there are issues decoding the ZIP.

--- app/src/main/java/com/hashapps/cadenas/workers/ModelDownloadWorker.kt
+++ app/src/main/java/com/hashapps/cadenas/workers/ModelDownloadWorker.kt
@@ -2,27 +2,27 @@ package com.hashapps.cadenas.workers
 
 import android.content.Context
 import androidx.core.app.NotificationCompat
-import androidx.work.CoroutineWorker
 import androidx.work.Data
 import androidx.work.ForegroundInfo
+import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.hashapps.cadenas.CadenasApplication
 import com.hashapps.cadenas.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.IOException
+import java.net.SocketTimeoutException
 import java.net.URL
 import java.util.zip.ZipException
 import java.util.zip.ZipInputStream
+import javax.net.ssl.HttpsURLConnection
 import kotlin.io.path.Path
 import kotlin.io.path.outputStream
 
 class ModelDownloadWorker(
     private val context: Context,
     params: WorkerParameters,
-) : CoroutineWorker(context, params) {
-    override suspend fun getForegroundInfo(): ForegroundInfo {
+) : Worker(context, params) {
+    override fun getForegroundInfo(): ForegroundInfo {
         val notification = NotificationCompat.Builder(context, CadenasApplication.CHANNEL_ID)
             .setContentTitle(context.getString(R.string.download_title))
             .setSmallIcon(R.drawable.baseline_downloading_24)
@@ -31,55 +31,87 @@ class ModelDownloadWorker(
         return ForegroundInfo(0, notification)
     }
 
-    override suspend fun doWork(): Result {
+    override fun doWork(): Result {
         val url = inputData.getString(KEY_MODEL_URL) ?: return Result.failure()
         val outDir = inputData.getString(KEY_MODEL_DIR) ?: return Result.failure()
 
-        var downloadSuccessful = false
-        val msg = withContext(Dispatchers.IO) {
-            try {
-                downloadModelTo(url, outDir)
-                downloadSuccessful = true
-                "Download complete!"
-            } catch (_: IOException) {
-                "An IO error occurred - please try again."
-            } catch (_: ZipException) {
-                "The model ZIP at $url is invalid."
-            } catch (_: java.lang.Exception) {
-                "An unknown error occurred."
-            }
-        }
-
-        val outData = Data.Builder().putString(KEY_RESULT_MSG, msg).build()
-        return if (downloadSuccessful) {
-            Result.success(outData)
-        } else {
-            Result.failure(outData)
-        }
+        return downloadModelTo(url, outDir)
     }
 
-    private fun downloadModelTo(url: String, outDir: String) {
+    private fun downloadModelTo(url: String, outDir: String): Result {
         val tempOutDir = File("$outDir.temp").also { it.mkdirs() }
 
-        ZipInputStream(URL(url).openConnection().inputStream).use { inStream ->
-            generateSequence { inStream.nextEntry }
-                .filterNot { it.isDirectory }
-                .forEach {
-                    Path(tempOutDir.path, it.name).outputStream().use { outStream ->
-                        inStream.copyTo(outStream)
-                    }
-                }
+        // TODO: We need to be smarter about some of these string literals for localization!
+        // TODO: Maybe we use some internal string constants to later choose the UI text?
+
+        val urlConnection = try {
+            URL(url).openConnection()
+        } catch (_: IOException) {
+            return fail("Something went wrong preparing a connection to $url. Please try again.")
         }
 
-        File(tempOutDir, URL_FILE).writeText(url)
+        // Sanity check: The UI should catch any non-HTTPS inputs.
+        require(urlConnection is HttpsURLConnection)
+
+        try {
+            urlConnection.connect()
+        } catch (_: SocketTimeoutException) {
+            return fail("The connection to $url timed out. Please try again.")
+        } catch (_: IOException) {
+            return fail("Something went wrong connecting to $url. Please try again.")
+        }
+
+        if (urlConnection.contentType != ZIP_MIME) {
+            return fail("$url does not refer to a ZIP. Please try again.")
+        }
+
+        val zipStream = try {
+            urlConnection.inputStream
+        } catch (_: IOException) {
+            return fail("Something went wrong creating an input stream for $url. Please try again.")
+        }
+        try {
+            ZipInputStream(zipStream).use { inStream ->
+                generateSequence { inStream.nextEntry }
+                    .filterNot { it.isDirectory }
+                    .forEach {
+                        Path(tempOutDir.path, it.name).outputStream().use { outStream ->
+                            inStream.copyTo(outStream)
+                        }
+                    }
+            }
+        } catch (_: ZipException) {
+            return fail("There was an error decoding the ZIP at $url. Please try again.")
+        } catch (_: IOException) {
+            return fail("Something went wrong reading the ZIP at $url. Please try again.")
+        }
 
         tempOutDir.renameTo(File(outDir))
+
+        return succeed()
+    }
+
+    private fun succeed(): Result {
+        return Result.success(
+            Data.Builder().putString(
+                KEY_RESULT_MSG,
+                "Model download successful!"
+            ).build()
+        )
+    }
+    private fun fail(msg: String): Result {
+        return Result.failure(
+            Data.Builder().putString(
+                KEY_RESULT_MSG,
+                msg
+            ).build()
+        )
     }
 
     companion object {
         const val KEY_MODEL_URL = "model_url"
         const val KEY_MODEL_DIR = "model_dir"
         const val KEY_RESULT_MSG = "msg"
-        private const val URL_FILE = ".url"
+        private const val ZIP_MIME = "application/zip"
     }
 }
