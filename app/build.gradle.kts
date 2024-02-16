2f7c7feece69b22930392dba2d86c960d567cac4
Chris Phifer <cphifer@galois.com>
2024-02-15 22:48:06 -0800

    Bump version number and code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "com.hashapps.cadenas"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
