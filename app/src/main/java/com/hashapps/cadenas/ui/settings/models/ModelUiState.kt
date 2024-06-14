89bfc6210267dd3df1dc2b99ee3bac77bdaca8e3
Chris Phifer <cphifer@galois.com>
2024-06-14 14:16:27 -0700

    ModelUiState: Update the sample model URL.

--- app/src/main/java/com/hashapps/cadenas/ui/settings/models/ModelUiState.kt
+++ app/src/main/java/com/hashapps/cadenas/ui/settings/models/ModelUiState.kt
@@ -3,7 +3,7 @@ package com.hashapps.cadenas.ui.settings.models
 import com.hashapps.cadenas.data.models.Model
 
 const val SAMPLE_MODEL_NAME = "microfic"
-const val SAMPLE_MODEL_URL = "https://owncloud.galois.com/index.php/s/wWN7x38TdaXEjUS/download"
+const val SAMPLE_MODEL_URL = "https://owncloud.galois.com/index.php/s/OCxmKKkJVl9Z9gX/download"
 
 /**
  * UI state for the model-adding screen.
