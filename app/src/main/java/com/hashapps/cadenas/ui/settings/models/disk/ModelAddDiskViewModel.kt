bcc10ea206ea49e0a619d83c61ac1e57e2572f33
Chris Phifer <cphifer@galois.com>
2024-07-11 15:18:59 -0700

    ModelAddDisk: Implement the view model boilerplate, reusing ModelUiState.

--- app/src/main/java/com/hashapps/cadenas/ui/settings/models/disk/ModelAddDiskViewModel.kt
+++ app/src/main/java/com/hashapps/cadenas/ui/settings/models/disk/ModelAddDiskViewModel.kt
@@ -1,9 +1,54 @@
 package com.hashapps.cadenas.ui.settings.models.disk
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hashapps.cadenas.data.models.ModelRepository
+import com.hashapps.cadenas.ui.settings.models.ModelUiState
+import com.hashapps.cadenas.ui.settings.models.isValid
+import com.hashapps.cadenas.ui.settings.models.toModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class ModelAddDiskViewModel(
     private val modelRepository: ModelRepository,
 ) : ViewModel() {
+    var modelUiState: ModelUiState by mutableStateOf(ModelUiState())
+        private set
+
+    val modelNames = modelRepository.getAllModelsStream().map { model ->
+        model.map { it.name }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = listOf(),
+    )
+
+    /**
+     * Update the model-add screen UI state, only enabling the install button
+     * if the new state is valid.
+     */
+    fun updateUiState(newModelUiState: ModelUiState) {
+        modelUiState =
+            newModelUiState.copy(actionEnabled = newModelUiState.isValid(modelNames.value))
+    }
+
+    /**
+     * If a valid name has been entered and a valid ZIP selected, start the
+     * model-installation worker.
+     */
+    fun installModel() {}
+
+    /**
+     * Save an installed model to the database.
+     */
+    fun saveModel(hash: String) {
+        viewModelScope.launch {
+            modelRepository.insertModel(modelUiState.toModel(hash))
+        }
+    }
 }
